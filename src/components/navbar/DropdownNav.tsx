c5394cfccbad73d87a790c3eb725cde76abd6548
Jarno Weemen <jarno.weemen@gmail.com>
2023-01-18 19:55:38 +0100

    Fixed bug Expected an assignment or function call and instead saw an expression. and some minor warnings.

--- src/components/navbar/DropdownNav.tsx
+++ src/components/navbar/DropdownNav.tsx
@@ -43,33 +43,38 @@ export default function DropdownNav() {
     return (
       <div>
         <div className="dropdown-content-left">
-          Currently no categories available
+          Category service down
         </div>
       </div>
     );
   }
 
   if (isLoading) {
-    <div className="dropdown-content-left">
-      <p>Loading...</p>
-    </div>;
+    return (
+      <div className="dropdown-content-left">
+        <p>Loading...</p>
+      </div>
+    );
   }
 
   return (
     <div className="dropdown hover:bg-orange-400 rounded">
-      <button className="dropdownbtn w-full p-2 flex items-center" onClick={toggleDropdown}>
+      <button
+        className="dropdownbtn w-full p-2 flex items-center"
+        onClick={toggleDropdown}
+      >
         Categories
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="w-5 h-5"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M19.5 8.25l-7.5 7.5-7.5-7.5"
           />
         </svg>
